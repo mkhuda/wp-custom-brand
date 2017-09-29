ff7b413127e9339b1f81a86d0ce22b53c0c41368
Muhammad K. Huda <mhmmd.huda@gmail.com>
2017-09-29 16:02:24 +0700

    add image uploader support for brand taxonomy

--- brands.js
+++ brands.js
@@ -1,8 +1,67 @@
 jQuery(function($){
+
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+  // For PRODUCT ///////////////////////////////
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+
     /*
      * Select/Upload image(s) event
      */
-    $('body').on('click', '.beacorp_brand_upload_image_button', function(e){
+    $('body').on('click', '.product_upload_image_button', function(e){
+        e.preventDefault();
+
+            var button = $(this),
+                custom_uploader = wp.media({
+            title: 'Insert Product Image',
+            library : {
+                // uncomment the next line if you want to attach image to the current post
+                // uploadedTo : wp.media.view.settings.post.id,
+                type : 'image'
+            },
+            button: {
+                text: 'Use this image' // button label text
+            },
+            multiple: false // for multiple image selection set to true
+        }).on('select', function() { // it also has "open" and "close" events
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            $(button).removeClass('button').html('<img class="true_pre_image" src="' + attachment.url + '" style="max-width:95%;display:block;" />').next().val(attachment.id).next().show();
+            /* if you sen multiple to true, here is some code for getting the image IDs
+            var attachments = frame.state().get('selection'),
+                attachment_ids = new Array(),
+                i = 0;
+            attachments.each(function(attachment) {
+                attachment_ids[i] = attachment['id'];
+                console.log( attachment );
+                i++;
+            });
+            */
+        })
+        .open();
+    });
+
+    /*
+     * Remove image event
+     */
+    $('body').on('click', '.product_remove_image_button', function(){
+        $(this).hide().prev().val(' ').prev().addClass('button').html('Upload image');
+        return false;
+    });
+
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    // For BRAND /////////////////////////////////
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    /*
+     * Select/Upload image(s) event
+     */
+    $('body').on('click', '.brand_upload_image_button', function(e){
         e.preventDefault();
 
             var button = $(this),
@@ -37,7 +96,7 @@ jQuery(function($){
     /*
      * Remove image event
      */
-    $('body').on('click', '.beacorp_brand_remove_image_button', function(){
+    $('body').on('click', '.brand_remove_image_button', function(){
         $(this).hide().prev().val(' ').prev().addClass('button').html('Upload image');
         return false;
     });
